35190e715482f659a482291c61c9a16dc7ef6c5f
zerocker <zerocloude@gmail.com>
2026-02-04 21:53:30 +0900

    Move DrawVertDeclaration to ImGuiService

--- FezEditor/Services/ImGuiService.cs
+++ FezEditor/Services/ImGuiService.cs
@@ -320,4 +320,29 @@ public partial class ImGuiService : IImGuiService
             _fontTexture.Dispose();
         }
     }
+    
+    private static class DrawVertDeclaration
+    {
+        public static readonly VertexDeclaration Declaration;
+
+        public static readonly int Size;
+
+        static DrawVertDeclaration()
+        {
+            unsafe { Size = sizeof(ImDrawVert); }
+
+            Declaration = new VertexDeclaration(
+                Size,
+
+                // Position
+                new VertexElement(0, VertexElementFormat.Vector2, VertexElementUsage.Position, 0),
+
+                // UV
+                new VertexElement(8, VertexElementFormat.Vector2, VertexElementUsage.TextureCoordinate, 0),
+
+                // Color
+                new VertexElement(16, VertexElementFormat.Color, VertexElementUsage.Color, 0)
+            );
+        }
+    }
 }
